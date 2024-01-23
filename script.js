821c96f6a42da6aa14dacedbe26b07609845c7cd
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-01-24 00:22:58 +0300

    Guess My Nuber

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 'use strict';
+const resetBtn = document.querySelector('.again');
+const num = document.querySelector('.number');
+const guessInput = Number(document.querySelector('.guess').value);
+const checkBtn = document.querySelector('.check');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+num.textContent = secretNumber;
+
+console.log(typeof guessInput);
+
+checkBtn.addEventListener('click', () => {
+
+    if (!guessInput) {
+        message.textContent = '⛔ No Number!';
+    } else if (guessInput == secretNumber) {
+        message.textContent = '🥳Correct Number!';
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
